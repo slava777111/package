00d41758bec3e3f425110958097fd957cd13dc71
slava777111 <slava777111@yandex.ru>
2017-09-21 13:15:24 +0400

    Added entry point

---
+++ src/PO52.Emelyanov.wdad/Application.java
@@ -0,0 +1,10 @@
+package PO52.Emelyanov.wdad;
+
+/**
+ * Created by U_LIVT35404 on 21.09.2017.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.print("I’m Vyacheslav Emelyanov, and I’m not a monkey");
+    }
+}
